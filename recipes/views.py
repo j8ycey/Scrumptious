72746b85c204f8893e85d4c82656f3f319dbf784
Joyce <j8ycey@gmail.com>
2022-06-07 13:14:24 -0700

    finished stretch goal challenge

--- recipes/views.py
+++ recipes/views.py
@@ -5,14 +5,8 @@ from django.views.generic.edit import CreateView, DeleteView, UpdateView
 from django.views.generic.list import ListView
 
 from recipes.forms import RatingForm
-
-# try:
-from recipes.forms import RecipeForm
+# from recipes.forms import RecipeForm
 from recipes.models import Recipe
-
-# except Exception:
-# RecipeForm = None
-# Recipe = None
 
 
 def log_rating(request, recipe_id):
