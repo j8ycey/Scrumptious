95c8d06cb1aadb42f80ef69c79e3f9ea2f31d698
Joyce <j8ycey@gmail.com>
2022-06-07 12:02:54 -0700

    removed unnecessary try-except blocks

--- recipes/forms.py
+++ recipes/forms.py
@@ -1,23 +1,16 @@
 from django import forms
-
-try:
-    from recipes.models import Recipe
-
-    class RecipeForm(forms.ModelForm):
-        class Meta:
-            model = Recipe
-            fields = [
-                "name",
-                "author",
-                "description",
-                "image",
-            ]
-
-except Exception:
-    pass
+from recipes.models import Recipe, Rating
 
 
-from recipes.models import Rating
+class RecipeForm(forms.ModelForm):
+    class Meta:
+        model = Recipe
+        fields = [
+            "name",
+            "author",
+            "description",
+            "image",
+        ]
 
 
 class RatingForm(forms.ModelForm):
